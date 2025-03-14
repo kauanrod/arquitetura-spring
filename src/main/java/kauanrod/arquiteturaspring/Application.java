b026b17e6a6035b68d2d780adaf9ee7208976241
Kauan Rodrigues <kauanrod@hotmail.com>
2025-03-14 13:43:08 -0300

    Configurações do builder, applicationContext e do ambiente

--- src/main/java/kauanrod/arquiteturaspring/Application.java
+++ src/main/java/kauanrod/arquiteturaspring/Application.java
@@ -1,13 +1,30 @@
 package kauanrod.arquiteturaspring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+
+        builder.bannerMode(Banner.Mode.OFF);
+        builder.profiles("producao", "homologacao");
+        builder.run(args);
+
+//        CONTEXTO DA APLICAÇÃO JÁ INICIADA
+        ConfigurableApplicationContext applicationContext = builder.context();
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String applicationName = environment.getProperty("spring.application.name");
+        System.out.println("Nome da aplicação: " + applicationName);
+
+        //        var produtoRepository = applicationContext.getBean("produtoRepository");
+//        SpringApplication.run(Application.class, args);
     }
 
 }
